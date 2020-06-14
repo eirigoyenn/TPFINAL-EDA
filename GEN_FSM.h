556a9af1d0de3915ab654e21f45cb7f8303d4134
Manu <mcorcos@itba.edu.ar>
2020-06-14 00:50:32 -0300

    ahi tiene q estare

--- GEN_FSM.h
+++ GEN_FSM.h
@@ -16,14 +16,14 @@ private:
 	const fsmCell fsmTable[4][6] = {
 		//     EVENTOS					PING								NETWORK NOT READY							NETWORK READY						NETWORK CREATED						NOEVENT								TIMER
 	/*estados*/
-		/*IDLE*/						{{idle,TX(RutaDefault)},			{idle,TX(RutaDefault)},						{idle,TX(RutaDefault)},				{idle,TX(RutaDefault)},				{idle,TX(noevent_r_acc)},				{idle,TX(RutaDefault)} },
-		/*WAITING LAYOUT*/				{{waitinglayout,TX(RutaDefault)},	{waitinglayout,TX(RutaDefault)},			{waitinglayout,TX(RutaDefault)},	{waitinglayout,TX(RutaDefault)},	{waitinglayout,TX(noevent_r_acc)},	{waitinglayout,TX(RutaDefault)} },
-		/*COLLECTING NET MEMBERS*/		{{collecting,TX(RutaDefault)},		{collecting,TX(RutaDefault)},				{collecting,TX(RutaDefault)},		{collecting,TX(RutaDefault)},		{collecting,TX(noevent_r_acc)},		{collecting,TX(RutaDefault)} },
-		/*NET CREATED*/					{{netcreated,TX(RutaDefault)},		{netcreated,TX(RutaDefault)},				{netcreated,TX(RutaDefault)},		{netcreated,TX(RutaDefault)},		{netcreated,TX(RutaDefault)},		{netcreated,TX(RutaDefault)} },
+		/*IDLE*/						{{idle,TX(RutaDefault)},			{idle,TX(RutaDefault)},						{idle,TX(RutaDefault)},				{idle,TX(RutaDefault)},				{idle,TX(Noevent_r_acc)},				{idle,TX(RutaDefault)} },
+		/*WAITING LAYOUT*/				{{waitinglayout,TX(RutaDefault)},	{waitinglayout,TX(RutaDefault)},			{waitinglayout,TX(RutaDefault)},	{waitinglayout,TX(RutaDefault)},	{waitinglayout,TX(Noevent_r_acc)},	{waitinglayout,TX(RutaDefault)} },
+		/*COLLECTING NET MEMBERS*/		{{collecting,TX(RutaDefault)},		{collecting,TX(RutaDefault)},				{collecting,TX(RutaDefault)},		{collecting,TX(RutaDefault)},		{collecting,TX(Noevent_r_acc)},		{collecting,TX(RutaDefault)} },
+		/*NET CREATED*/					{{netcreated,TX(RutaDefault)},		{netcreated,TX(RutaDefault)},				{netcreated,TX(RutaDefault)},		{netcreated,TX(RutaDefault)},		{netcreated,TX(Noevent_r_acc)},		{netcreated,TX(RutaDefault)} },
 
 	};
 	void RutaDefault(genericEvent* ev);
-	void noevent_r_acc(genericEvent* ev);
+	void Noevent_r_acc(genericEvent* ev);
 \
 	states State;
 };
